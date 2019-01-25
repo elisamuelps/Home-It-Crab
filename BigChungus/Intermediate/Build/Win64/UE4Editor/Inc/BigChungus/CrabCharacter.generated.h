32cad9b2d37af1a708533b0cac6babb2de9e29a7
Eli Menendez <elisamuelpantoja@gmail.com>
2019-01-25 19:46:59 +0000

    Conver Project to C++ Project

---
+++ BigChungus/Intermediate/Build/Win64/UE4Editor/Inc/BigChungus/CrabCharacter.generated.h
@@ -0,0 +1,88 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+/*===========================================================================
+	Generated code exported from UnrealHeaderTool.
+	DO NOT modify this manually! Edit the corresponding .h files instead!
+===========================================================================*/
+
+#include "UObject/ObjectMacros.h"
+#include "UObject/ScriptMacros.h"
+
+PRAGMA_DISABLE_DEPRECATION_WARNINGS
+#ifdef BIGCHUNGUS_CrabCharacter_generated_h
+#error "CrabCharacter.generated.h already included, missing '#pragma once' in CrabCharacter.h"
+#endif
+#define BIGCHUNGUS_CrabCharacter_generated_h
+
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_RPC_WRAPPERS
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_INCLASS_NO_PURE_DECLS \
+private: \
+	static void StaticRegisterNativesACrabCharacter(); \
+	friend struct Z_Construct_UClass_ACrabCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ACrabCharacter, ACharacter, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/BigChungus"), NO_API) \
+	DECLARE_SERIALIZER(ACrabCharacter)
+
+
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_INCLASS \
+private: \
+	static void StaticRegisterNativesACrabCharacter(); \
+	friend struct Z_Construct_UClass_ACrabCharacter_Statics; \
+public: \
+	DECLARE_CLASS(ACrabCharacter, ACharacter, COMPILED_IN_FLAGS(0), CASTCLASS_None, TEXT("/Script/BigChungus"), NO_API) \
+	DECLARE_SERIALIZER(ACrabCharacter)
+
+
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_STANDARD_CONSTRUCTORS \
+	/** Standard constructor, called after all reflected properties have been initialized */ \
+	NO_API ACrabCharacter(const FObjectInitializer& ObjectInitializer); \
+	DEFINE_DEFAULT_OBJECT_INITIALIZER_CONSTRUCTOR_CALL(ACrabCharacter) \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrabCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrabCharacter); \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrabCharacter(ACrabCharacter&&); \
+	NO_API ACrabCharacter(const ACrabCharacter&); \
+public:
+
+
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+	/** Private move- and copy-constructors, should never be used */ \
+	NO_API ACrabCharacter(ACrabCharacter&&); \
+	NO_API ACrabCharacter(const ACrabCharacter&); \
+public: \
+	DECLARE_VTABLE_PTR_HELPER_CTOR(NO_API, ACrabCharacter); \
+DEFINE_VTABLE_PTR_HELPER_CTOR_CALLER(ACrabCharacter); \
+	DEFINE_DEFAULT_CONSTRUCTOR_CALL(ACrabCharacter)
+
+
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_PRIVATE_PROPERTY_OFFSET
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_9_PROLOG
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_GENERATED_BODY_LEGACY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_RPC_WRAPPERS \
+	BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_INCLASS \
+	BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_STANDARD_CONSTRUCTORS \
+public: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#define BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_GENERATED_BODY \
+PRAGMA_DISABLE_DEPRECATION_WARNINGS \
+public: \
+	BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_PRIVATE_PROPERTY_OFFSET \
+	BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_RPC_WRAPPERS_NO_PURE_DECLS \
+	BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_INCLASS_NO_PURE_DECLS \
+	BigChungus_Source_BigChungus_Public_CrabCharacter_h_12_ENHANCED_CONSTRUCTORS \
+private: \
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
+
+
+#undef CURRENT_FILE_ID
+#define CURRENT_FILE_ID BigChungus_Source_BigChungus_Public_CrabCharacter_h
+
+
+PRAGMA_ENABLE_DEPRECATION_WARNINGS
